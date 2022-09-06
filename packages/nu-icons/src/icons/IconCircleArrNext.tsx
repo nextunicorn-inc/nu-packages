82b489046c359f2489c4b6c6c650b7e45623e5fb
siny <dltlsgh5@gmail.com>
2022-09-06 10:23:13 +0900

    [FEAT] Icon 추가(dash,circleArrNext,CheckSelected,Attachment

---
+++ packages/nu-icons/src/icons/IconCircleArrNext.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { convertIcon, IconSize } from '../component/Icon';
+
+function SvgComponent({ color = '#616161', ...rest }: React.SVGProps<SVGSVGElement> & IconSize) {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="12" cy="12" r="9.5" stroke={color} />
+      <path
+        d="M10.5 17L15.5 12L10.5 7"
+        stroke={color}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+const IconComponent = convertIcon(SvgComponent, 'circle-arrNext');
+export default IconComponent;
