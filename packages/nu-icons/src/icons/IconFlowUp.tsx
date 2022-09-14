f7f00299f0cf2a44fa54297842d28f6adea06420
InSeong-So <goflvhxj2547@gmail.com>
2022-09-14 17:05:38 +0900

    Fix: throw new Error 제거

--- packages/nu-icons/src/icons/IconFlowUp.tsx
+++ packages/nu-icons/src/icons/IconFlowUp.tsx
@@ -2,37 +2,28 @@ import React from 'react';
 import { convertIcon, IconSize } from '../component/Icon';
 
 function SvgComponent({
-  size = '20',
+  size = '16',
   color = '#FF3334',
   ...rest
 }: React.SVGProps<SVGSVGElement> & IconSize) {
-  if (size === '16') {
-    return (
-      <svg
-        width="16"
-        height="16"
-        viewBox="0 0 16 16"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M8.00024 12.0002V4"
-          stroke={color}
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path
-          d="M4 8.00009L8.00009 4L12.0002 8.00009"
-          stroke={color}
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
-    );
-  }
-  throw new Error(`${size}는 피그마에 없는 사이즈 입니다.`);
+  return (
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M8.00024 12.0002V4"
+        stroke={color}
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M4 8.00009L8.00009 4L12.0002 8.00009"
+        stroke={color}
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
 }
 
 const IconComponent = convertIcon(SvgComponent, 'flow-up');
