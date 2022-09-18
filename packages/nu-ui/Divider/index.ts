0d77649e92d2d9a63194b5eb78f3d903713ab9b6
InSeong-So <goflvhxj2547@gmail.com>
2022-09-18 20:23:01 +0900

    Feat: 범용 컴포넌트 Divider 추가(#9)

---
+++ packages/nu-ui/Divider/index.ts
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+import { Color } from '../@foundations';
+
+const Divider = styled.hr`
+  border: 0.5px solid ${Color.angelsGray1};
+`;
+
+export default Divider;
