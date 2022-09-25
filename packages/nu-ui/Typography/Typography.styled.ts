b456312b881aa57f592a7b0524bdbd9268746774
InSeong-So <goflvhxj2547@gmail.com>
2022-09-25 18:07:09 +0900

    Fix: 잘못 들어간 font-weight 수정

--- packages/nu-ui/Typography/Typography.styled.ts
+++ packages/nu-ui/Typography/Typography.styled.ts
@@ -5,7 +5,7 @@ import type TypoProps from './Typography.types';
 
 export const TypographyWrapper = styled.span<TypoProps>`
   ${({ size }) => (size ? FontSize[size] : FontSize.p1)};
-  font-weight: ${({ weight }) => weight && FontWeight[weight]};
+  ${({ weight }) => weight && FontWeight[weight]};
   ${({ color }) => color && `color: ${color}`};
   letter-spacing: ${({ size }) => (size && size.includes('angels') ? '0' : '-0.04em')};
 `;
