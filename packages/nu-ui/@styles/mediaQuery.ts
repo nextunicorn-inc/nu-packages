9da5e4da5633464d5041665d3d7b9540be23dd89
InSeong-So <goflvhxj2547@gmail.com>
2022-10-20 15:17:34 +0900

    Fix: mediaQuery의 min-width 연산에 1px을 더함

--- packages/nu-ui/@styles/mediaQuery.ts
+++ packages/nu-ui/@styles/mediaQuery.ts
@@ -2,6 +2,6 @@ import { BrakePointTypes } from '../@constants';
 
 export const maxWidthQuery = (breakPoint: BrakePointTypes) => `(max-width: ${breakPoint}px)`;
 
-export const minWidthQuery = (breakPoint: BrakePointTypes) => `(min-width: ${breakPoint}px)`;
+export const minWidthQuery = (breakPoint: BrakePointTypes) => `(min-width: ${breakPoint + 1}px)`;
 
 export const mediaQuery = (breakPoint: BrakePointTypes) => `@media ${maxWidthQuery(breakPoint)}`;
