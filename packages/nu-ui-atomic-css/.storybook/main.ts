509d5dbaafa322e201466add1453ff761a62cac5
InSeong-So <goflvhxj2547@gmail.com>
2022-11-23 16:59:06 +0900

    Chore: atom-css 테스트 패키지 추가

---
+++ packages/nu-ui-atomic-css/.storybook/main.ts
@@ -0,0 +1,48 @@
+module.exports = {
+  core: {
+    builder: 'webpack5',
+  },
+  stories: ['../**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  logLevel: 'debug',
+  reactOptions: {
+    fastRefresh: true,
+  },
+  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: '@storybook/react',
+  typescript: {
+    check: true,
+  },
+  features: {
+    babelModeV7: true,
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve('babel-loader'),
+    });
+
+    config.module.rules.push({
+      test: /\.module\.css$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+          },
+        },
+      ],
+    });
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    config.module.rules.push({
+      resolve: { fullySpecified: false },
+    });
+
+    return config;
+  },
+};
---
+++ packages/nu-ui-atomic-css/webpack.config.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const webpack = require('webpack');
+const DefinePlugin = webpack.DefinePlugin;
+const TerserPlugin = require('terser-webpack-plugin');
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+const getBabelConfig = require('./getBabelConfig');
+
+const rootPath = path.join(__dirname, '../..');
+module.exports = function ({ minimize }) {
+  return {
+    mode: 'production',
+    bail: true,
+    devtool: 'source-map',
+    entry: {
+      index: ['./index.ts'],
+    },
+    output: {
+      filename: minimize ? 'ui.min.js' : 'ui.js',
+      path: path.join(__dirname, 'dist/umd'),
+      library: 'NextuniconUI',
+      libraryTarget: 'umd',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          include: [
+            path.join(rootPath, 'packages/nu-types'),
+            path.join(rootPath, 'packages/nu-ui'),
+          ],
+          use: [
+            {
+              loader: 'babel-loader',
+              options: getBabelConfig({ isESM: true }),
+            },
+            {
+              loader: 'ts-loader',
+              options: {
+                transpileOnly: true,
+                happyPackMode: false,
+                appendTsSuffixTo: [],
+              },
+            },
+          ],
+        },
+      ],
+    },
+    optimization: {
+      minimize: !!minimize,
+      minimizer: [new TerserPlugin()],
+    },
+    performance: { maxEntrypointSize: 10485760, maxAssetSize: 5242880 },
+    plugins: [
+      new DefinePlugin({
+        'process.env': { NODE_ENV: '"production"', PUBLIC_URL: undefined },
+      }),
+      new CaseSensitivePathsPlugin(),
+      new webpack.ids.HashedModuleIdsPlugin({
+        context: __dirname,
+        hashFunction: 'sha256',
+        hashDigest: 'hex',
+        hashDigestLength: 20,
+      }),
+    ],
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+      alias: {
+        '@nextunicorn/types': path.resolve(__dirname, '../nu-types'),
+      },
+    },
+    externals: {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react',
+      },
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom',
+      },
+    },
+  };
+};
